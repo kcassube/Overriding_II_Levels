4585e9ce120856c6ebf0c0a6e4c54889e6f928b3
kcassube <79251426+kcassube@users.noreply.github.com>
2023-08-02 08:42:51 +0200

    01_Class_Stuff

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,6 +5,12 @@ public class App {
     
         Player player = new Player();
 
+        // output(player.play());
+        // output(player.play());
+        // output(player.play());
+        // output(player.play());
+        // output(player.play());
+        // output(player.play());
         
         // Variante 1
         // for (int i = 0; i < 12; i++) {
@@ -12,9 +18,9 @@ public class App {
         // }
 
         // Variante 2
-        // while (player.points < 13) {
-        //     output(player.play()); 
-        // }
+        while (player.points < 13) {
+            output(player.play()); 
+        }
 
 
        
